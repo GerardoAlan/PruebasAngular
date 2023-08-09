fe38783e4e1d1417edf4fb09f8442f4c2eebf9e5
GerardoAlan <angui_gerard@hotmail.com>
2023-08-09 15:03:10 -0600

    Update app.component.spec.ts

--- angular-project/src/app/app.component.spec.ts
+++ angular-project/src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 // AppComponent es el nombre de la clase a probar
 describe('AppComponent', () => {
@@ -10,7 +10,8 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        ReactiveFormsModule,
       ],
       declarations: [
         AppComponent
